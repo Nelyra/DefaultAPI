174270ed424e48a18ce36d061028a8efb7125ef9
Namu <49788407+namuuu@users.noreply.github.com>
2025-06-03 16:04:44 +0200

    Added bb_plus_money.sql for testing

--- bdd/bb_plus_money.sql
+++ bdd/bb_plus_money.sql
@@ -1,7 +1,5 @@
-
-DROP Table IF EXISTS Categorie;
-
-create table if not exists Categorie
+DROP TABLE IF EXISTS Categorie;
+CREATE TABLE Categorie
 (
     idCategorie       int auto_increment
         primary key,
@@ -10,26 +8,42 @@ create table if not exists Categorie
     dateHeureMAJ      timestamp default current_timestamp() not null
 );
 
+DELIMITER $$
+CREATE TRIGGER TRG_BEFORE_UPDATE_CATEGORIE
+    before update
+    on Categorie
+    for each row
+begin
+    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
+end$$
+DELIMITER ;
 
-DROP Table IF EXISTS SousCategorie;
-
-create table if not exists SousCategorie
+DROP TABLE IF EXISTS SousCategorie;
+CREATE TABLE SousCategorie
 (
     idSousCategorie   int auto_increment
         primary key,
     nomSousCategorie  varchar(50)                           not null,
-    idCategorie       int                                   not null,
+    idcategorie       int                                   not null,
     dateHeureCreation timestamp default current_timestamp() not null,
     dateHeureMAJ      timestamp default current_timestamp() not null,
     constraint SousCategorie_Categorie_idCategorie_fk
-        foreign key (idCategorie) references Categorie (idCategorie)
+        foreign key (idcategorie) references Categorie (idCategorie)
             on delete cascade
 );
 
+DELIMITER $$
+CREATE TRIGGER TRG_BEFORE_UPDATE_SOUS_CATEGORIE
+    before update
+    on SousCategorie
+    for each row
+begin
+    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
+end$$
+DELIMITER ;
 
-DROP Table IF EXISTS Utilisateur;
-
-create table if not exists Utilisateur
+DROP TABLE IF EXISTS Utilisateur;
+CREATE TABLE Utilisateur
 (
     idUtilisateur     int auto_increment
         primary key,
@@ -44,10 +58,8 @@ create table if not exists Utilisateur
     codePostal        char(5)                               null
 );
 
-
-DROP Table IF EXISTS Compte;
-
-create table if not exists Compte
+DROP TABLE IF EXISTS Compte;
+CREATE TABLE Compte
 (
     idCompte          int auto_increment
         primary key,
@@ -63,10 +75,18 @@ create table if not exists Compte
             on delete cascade
 );
 
+DELIMITER $$
+CREATE TRIGGER TRG_BEFORE_UPDATE_COMPTE
+    before update
+    on Compte
+    for each row
+begin
+    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
+end$$
+DELIMITER ;
 
-DROP Table IF EXISTS Tiers;
-
-create table if not exists Tiers
+DROP TABLE IF EXISTS Tiers;
+CREATE TABLE Tiers
 (
     idTiers           int auto_increment
         primary key,
@@ -78,16 +98,35 @@ create table if not exists Tiers
         foreign key (idUtilisateur) references Utilisateur (idUtilisateur)
 );
 
-DROP Table IF EXISTS Virement;
+DELIMITER $$
+CREATE TRIGGER TRG_BEFORE_UPDATE_TIERS
+    before update
+    on Tiers
+    for each row
+begin
+    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
+end$$
+DELIMITER ;
 
-create table if not exists Virement
+DELIMITER $$
+CREATE TRIGGER TRG_BEFORE_UPDATE_UTILISATEUR
+    before update
+    on Utilisateur
+    for each row
+begin
+    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
+end$$
+DELIMITER ;
+
+DROP TABLE IF EXISTS Virement;
+CREATE TABLE Virement
 (
     idVirement        int auto_increment
         primary key,
     idCompteDebit     int                                       not null,
     idCompteCredit    int                                       not null,
     montant           decimal(6, 2) default 0.00                not null,
-    dateVirement      date          default curdate()           not null,
+    dateVirement      date          default (CURRENT_DATE)      not null,
     dateHeureCreation timestamp     default current_timestamp() not null,
     dateHeureMAJ      timestamp     default current_timestamp() not null,
     idTiers           int                                       null,
@@ -98,20 +137,18 @@ create table if not exists Virement
         foreign key (idCompteCredit) references Compte (idCompte)
 );
 
-DROP Table IF EXISTS Mouvement;
-
-create table if not exists Mouvement
+DROP TABLE IF EXISTS Mouvement;
+CREATE TABLE Mouvement
 (
-    idMouvement       int auto_increment
-        primary key,
-    dateMouvement     date      default curdate()           not null,
+    idMouvement       int auto_increment                    primary key,
+    dateMouvement     date      default (CURRENT_DATE)      not null,
     idCompte          int                                   not null,
     idTiers           int       default 1                   null,
     idCategorie       int       default 1                   null,
     idSousCategorie   int                                   null,
     idVirement        int                                   null,
     montant           decimal(6, 2)                         null,
-    typeMouvement     char      default 'D'                 null,
+    typeMouvement     char                                  null,
     dateHeureCreation timestamp default current_timestamp() not null,
     dateHeureMAJ      timestamp default current_timestamp() not null,
     constraint Mouvement_Categorie_idCategorie_fk
@@ -123,137 +160,64 @@ create table if not exists Mouvement
         foreign key (idTiers) references Tiers (idTiers),
     constraint Mouvement_Virement_idVirement_fk
         foreign key (idVirement) references Virement (idVirement)
-            on update cascade on delete set null
+            on update cascade on delete set null,
+    constraint Mouvement_Compte_idCompte_fk
+        foreign key (idCompte) references Compte(idCompte)
 );
 
+DELIMITER $$
+CREATE TRIGGER TRG_BEFORE_INSERT_MOUVEMENT
+BEFORE INSERT ON Mouvement
+FOR EACH ROW
+BEGIN
+    DECLARE v_Categorie INT DEFAULT 0;
 
-drop view if exists V_CATEGORIE;
-create view V_CATEGORIE as
-select `c`.`nomCategorie` AS `nomCategorie`, `sc`.`nomSousCategorie` AS `nomSousCategorie`
-from Categorie `c`
-         join SousCategorie `sc`
-where `sc`.`idcategorie` = `c`.`idCategorie`
-order by `c`.`nomCategorie`, `sc`.`nomSousCategorie`;
+    -- Vérification que la sous-catégorie appartient bien à la catégorie
+    IF NEW.idSousCategorie IS NOT NULL THEN
+        SELECT idCategorie INTO v_Categorie 
+        FROM SousCategorie 
+        WHERE idSousCategorie = NEW.idSousCategorie;
 
-drop view if exists V_MOUVEMENT;
+        IF v_Categorie <> NEW.iDCategorie THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = "La sous-catégorie n\'appartient pas à la catégorie choisie";
+        END IF;
+    END IF;
 
-create view V_MOUVEMENT as
-select `m`.`idMouvement`         AS `idMouvement`,
-       `m`.`dateMouvement`       AS `dateMouvement`,
-       `c`.`descriptionCompte`   AS `descriptionCompte`,
-       `c`.`nomBanque`           AS `nomBanque`,
-       `t`.`nomTiers`            AS `nomTiers`,
-       `ctg`.`nomCategorie`      AS `nomCategorie`,
-       `sctg`.`nomSousCategorie` AS `nomSousCategorie`,
-       `m`.`montant`             AS `montant`
-from ((((mouvement `m` join compte `c`
-         on (`m`.`idCompte` = `c`.`idCompte`)) join tiers `t`
-        on (`m`.`idTiers` = `t`.`idTiers`)) join categorie `ctg`
-       on (`m`.`idCategorie` = `ctg`.`idCategorie`)) left join souscategorie `sctg`
-      on (`m`.`idSousCategorie` = `sctg`.`idSousCategorie`))
-order by `m`.`dateMouvement`;
-
-DROP TRIGGER IF EXISTS TRG_BEFORE_UPDATE_CATEGORIE;
-DROP TRIGGER IF EXISTS TRG_BEFORE_UPDATE_SOUS_CATEGORIE;
-DROP TRIGGER IF EXISTS TRG_BEFORE_UPDATE_TIERS;
-DROP TRIGGER IF EXISTS TRG_BEFORE_UPDATE_UTILISATEUR;
-DROP TRIGGER IF EXISTS TRG_BEFORE_INSERT_MOUVEMENT;
-DROP TRIGGER IF EXISTS TRG_BEFORE_UPDATE_VIREMENT;
-DROP TRIGGER IF EXISTS TRG_AFTER_INSERT;
-DROP TRIGGER IF EXISTS TRG_AFTER_DELETE_VIREMENT;
+    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
+END$$
+DELIMITER ;
 
 DELIMITER $$
-create trigger TRG_BEFORE_UPDATE_CATEGORIE
-    before update
-    on Categorie
+CREATE TRIGGER TRG_BEFORE_INSERT_MOUVEMENT2
+    BEFORE INSERT ON Mouvement
     for each row
 begin
-    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
-end;
+    IF typeMouvement = 'D' THEN
+        UPDATE Compte
+        SET dernierSolde = dernierSolde - NEW.montant
+        WHERE idCompte = NEW.idCompte;
+    END IF; 
+    IF typeMouvement = 'C' THEN
+        UPDATE Compte
+        SET dernierSolde = dernierSolde + NEW.montant
+        WHERE idCompte = NEW.idCompte;
+    END IF;
+end$$
+DELIMITER ;
 
-$$
-
-create trigger TRG_BEFORE_UPDATE_SOUS_CATEGORIE
-    before update
-    on SousCategorie
-    for each row
-begin
-    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
-end;
-
-$$
-
-create trigger TRG_BEFORE_UPDATE_COMPTE
-    before update
-    on Compte
-    for each row
-begin
-    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
-end;
-
-$$
-
-create trigger TRG_BEFORE_UPDATE_TIERS
-    before update
-    on Tiers
-    for each row
-begin
-    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
-end;
-
-$$
-
-create trigger TRG_BEFORE_UPDATE_UTILISATEUR
-    before update
-    on Utilisateur
-    for each row
-begin
-    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
-end;
-
-$$
-
-create trigger TRG_BEFORE_INSERT_MOUVEMENT
-    before insert
-    on Mouvement
-    for each row
-begin
-    DEClARE v_Categorie INT DEFAULT 0;
-
-    /* Il faut vérifier que la sous-catégorie appartient bien à la catégorie */
-    IF NEW.idSousCategorie IS NOT NULL THEN
-        SELECT idCategorie INTO v_Categorie FROM SousCategorie WHERE idSousCategorie = NEW.idSousCategorie;
-        IF v_Categorie <> NEW.idCategorie THEN
-            SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'La sous-catégorie n\'appartient pas à la catégorie choisie';
-        end if;
-    end if;
-    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
-end;
-
-$$
-
-create trigger TRG_BEFORE_UPDATE_VIREMENT
-    before update
-    on Virement
-    for each row
-begin
-    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
-end;
-
-$$
-
-create trigger TRG_AFTER_DELETE_VIREMENT
+DELIMITER $$
+CREATE TRIGGER TRG_AFTER_DELETE_VIREMENT
     after delete
     on Virement
     for each row
 begin
     DELETE FROM Mouvement WHERE idVirement = OLD.idVirement;
-end;
+end$$
+DELIMITER ;
 
-$$
-
-create trigger TRG_AFTER_INSERT
+DELIMITER $$
+CREATE TRIGGER TRG_AFTER_INSERT
     after insert
     on Virement
     for each row
@@ -263,6 +227,39 @@ begin
 /* Un mouvement au crédit sur le cmpte crédité */
 INSERT INTO Mouvement(idCompte,montant,typeMouvement,idVirement,dateMouvement) VALUES (NEW.idCompteDebit,(NEW.montant * -1),'D',NEW.idVirement,NEW.dateVirement);
 INSERT INTO Mouvement(idCompte,montant,typeMouvement,idVirement,dateMouvement) VALUES ( NEW.idCompteCredit,NEW.montant, 'C',NEW.idVirement,NEW.dateVirement);
-end;
+end$$
+DELIMITER ;
 
-$$
+DELIMITER $$
+CREATE TRIGGER TRG_BEFORE_UPDATE_VIREMENT
+    before update
+    on Virement
+    for each row
+begin
+    SET NEW.dateHeureMAJ = CURRENT_TIMESTAMP;
+end$$
+DELIMITER ;
+
+CREATE OR REPLACE VIEW V_CATEGORIE as
+select `c`.`nomCategorie` AS `nomCategorie`, `sc`.`nomSousCategorie` AS `nomSousCategorie`
+from Categorie `c`
+         join SousCategorie `sc`
+where `sc`.`idcategorie` = `c`.`idCategorie`
+order by `c`.`nomCategorie`, `sc`.`nomSousCategorie`;
+
+CREATE OR REPLACE VIEW V_MOUVEMENT as
+select `m`.`idMouvement`         AS `idMouvement`,
+       `m`.`dateMouvement`       AS `dateMouvement`,
+       `c`.`descriptionCompte`   AS `descriptionCompte`,
+       `c`.`nomBanque`           AS `nomBanque`,
+       `t`.`nomTiers`            AS `nomTiers`,
+       `ctg`.`nomCategorie`      AS `nomCategorie`,
+       `sctg`.`nomSousCategorie` AS `nomSousCategorie`,
+       `m`.`montant`             AS `montant`
+from ((((Mouvement `m` join Compte `c`
+         on (`m`.`idCompte` = `c`.`idCompte`)) join Tiers `t`
+        on (`m`.`idTiers` = `t`.`idTiers`)) join Categorie `ctg`
+       on (`m`.`idCategorie` = `ctg`.`idCategorie`)) left join SousCategorie `sctg`
+      on (`m`.`idSousCategorie` = `sctg`.`idSousCategorie`))
+order by `m`.`dateMouvement`;
+
